699b94fb746d0e9c3b67dfaa1160a732990d16a0
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-05-26 15:59:56 +0200

    Component Accueil elements

---
+++ portfolio-osee/src/components/Accueil/index.js
@@ -0,0 +1,23 @@
+import './style.css';
+
+const Accueil = () => {
+  return (
+    <div className="Accueil">
+      <header>
+        <p>Logo</p>
+        <menu>
+          <p>Accueil</p>
+          <p>Présentation</p>
+          <p>Compétences</p>
+          <p>Contact</p>
+        </menu>
+      </header>
+      <h1>Osée Ralantoarison </h1>
+      <h2>Développeur web</h2>
+      <p>Photo de profil</p>
+      <p>Linkedin</p>
+    </div>
+  )
+};
+
+export default Accueil;
