55bdc6de80578cbfa897fa58ee11d225a84bee1b
Osée Ralantoarison <oralantoarison@greenweez.com>
2023-09-14 11:02:38 +0200

    refactor: competences

--- portfolio-osee/src/components/Competences/index.js
+++ portfolio-osee/src/components/Competences/index.js
@@ -1,25 +1,30 @@
-import React from 'react';
-
-import './style.css';
+import React from "react";
+import "./style.css";
 
 const Competences = () => {
+  const items = [
+    "HTML",
+    "CSS/SCSS",
+    "JAVASCRIPT",
+    "REACT",
+    "REDUX",
+    "NODE",
+    "NEXT",
+    "Git/Github",
+    "PostgreSQL",
+    "MONGODB",
+  ];
+
   return (
     <div className="Competences" id="competences">
       <h2 className="Competences__title">Compétences</h2>
       <div className="Competences__list">
-        <p className="Competences__item">HTML</p>
-        <p className="Competences__item">CSS</p>
-        <p className="Competences__item">Javascript</p>
-        <p className="Competences__item">React</p>
-        <p className="Competences__item">Redux</p>
-        <p className="Competences__item">Material-ui</p>
-        <p className="Competences__item">Node</p>
-        <p className="Competences__item">Express</p>
-        <p className="Competences__item">PostgreSQL</p>
-        <p className="Competences__item">Git/Github</p>
+        {items.map((item) => (
+          <p className="Competences__item">{item}</p>
+        ))}
       </div>
     </div>
-  )
+  );
 };
 
 export default Competences;
